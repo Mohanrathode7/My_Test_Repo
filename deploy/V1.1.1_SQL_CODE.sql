a4204314401604c408c9cfde574433a793ef0b2c
Mohanrathode7 <151394832+Mohanrathode7@users.noreply.github.com>
2024-05-11 12:34:42 +0530

    Update V1.1.1_SQL_CODE.sql

---
+++ deploy/V1.1.1_SQL_CODE.sql
@@ -0,0 +1,12 @@
+create or replace table emp(   
+  empno    number(4,0),   
+  ename    varchar2(10),   
+  job      varchar2(9),   
+  mgr      number(4,0),   
+  hiredate date,   
+  sal      number(7,2),   
+  comm     number(7,2),   
+  deptno   number(2,0)
+);
+
+create or replace table mohan (id number);
